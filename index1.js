cf2ec1a57c4d50df90397e566fb36f4cd51e5b18
Anastasiya-Baranouskaya <nastenkabv@gmail.com>
2021-08-27 23:48:29 +0200

    решение

---
+++ index1.js
@@ -0,0 +1,11 @@
+// task 1
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(ms), ms);
+  });
+};
+
+// Вызовы функции для проверки
+delay(2000).then((ms) => console.log(`Resolved after ${ms}`)); // Resolved after 2000ms
+delay(1000).then((ms) => console.log(`Resolved after ${ms}`)); // Resolved after 1000ms
+delay(1500).then((ms) => console.log(`Resolved after ${ms}`)); // Resolved after 1500ms
